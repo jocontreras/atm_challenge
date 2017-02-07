5aa378e669e40df98ca6939e1202af8844a58a83
jocontreras <jen.almarasy@gmail.com>
2017-02-07 22:35:11 +0100

    testing sad path

---
+++ lib/atm.rb
@@ -0,0 +1,54 @@
+class Atm
+
+  attr_accessor :funds
+
+  def initialize
+    @funds= 1000
+  end
+
+  def withdraw(amount, pin_code, account)
+  case
+  when insufficient_funds_in_account?(amount, account)
+    { status: false, message: 'insufficient funds in account', date: Date.today}
+  when insufficient_funds_in_atm?(amount)
+    { status: false, message: 'insufficient funds in ATM', date: Date.today}
+  when incorrect_pin?(pin_code, account.pin_code)
+    {status:false, message:'wrong pin', date: Date.today}
+  when card_expired?(account.exp_date)
+    {status:false, message:'card expired', date: Date.today}
+  when account_disabled?(account.account_status)
+    {status:false, message: 'account disabled', date: Date.today}
+  else
+      perform_transaction(amount,account)
+  end
+end
+
+end
+
+  private
+
+  def insufficient_funds_in_account?(amount,account)
+      amount > account.balance
+  end
+
+  def insufficient_funds_in_atm?(amount)
+   @funds < amount
+  end
+
+  def perform_transaction(amount,account)
+    @funds -= amount
+    account.balance = account.balance - amount
+     { status: true, message: 'success', date: Date.today, amount: amount }
+  end
+
+  def incorrect_pin?(pin_code, actual_pin)
+    pin_code != actual_pin
+  end
+
+  def card_expired?(exp_date)
+    Date.strptime(exp_date, '%m/%y') < Date.today
+  end
+
+  def account_disabled?(account_status)
+    account_status != 'active'
+  end
